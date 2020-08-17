7539d0f1d2c9990b27173e832bfdb8522456efd6
shengxuesun <admin@lets8.cn>
2020-08-17 15:28:36 +0800

    commit again

--- config/app.php
+++ config/app.php
@@ -39,7 +39,8 @@ return [
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'   => app()->isDebug(),
+//    'show_error_msg'   => app()->isDebug(),
+    'show_error_msg'   => false,
     // 当前应用版本号
     'thinkadmin_ver'   => 'v6',
 ];
